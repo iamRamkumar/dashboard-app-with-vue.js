868745eca38294c93e5f2f48c0e7f47ebf5fd67f
Ramkumar <ramk2394@gmail.com>
2019-02-25 12:59:15 +0530

    added Dashboard Page and route to Dashboard

---
+++ src/pages/Dashboard/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dashboard">
+      <b-row>
+        <b-col lg="1" xs="12" >
+          <SideNav />
+        </b-col>
+        <b-col lg="11" xs="12">
+          <Header />
+          1 of 3
+        </b-col>
+      </b-row>
+  </div>
+</template>
+
+<script>
+  import Header from '@/components/Header';
+  import SideNav from '@/components/SideNav';
+  export default {
+    name: 'Dashboard',
+    components: {Header}
+  }
+</script>
+
+<style scoped>
+
+</style>
