f36258273aaff911e7b314af513481fce0b5746d
Ramkumar <ramk2394@gmail.com>
2019-02-25 15:53:57 +0530

    Renamed User Page component

--- src/pages/UsersPage/UsersPage.vue
+++ src/pages/UsersPage/UsersPage.vue
@@ -7,14 +7,18 @@
       <div class="w-100"></div>
       <b-col>
         <b-alert variant="success" show>This Page will show Users</b-alert>
+        <Users />
+        <b-alert variant="success" show>This Page will show Users</b-alert>
       </b-col>
     </b-row>
   </div>
 </template>
 
 <script>
+  import Users from '@/components/Users';
   export default {
-    name: 'Users'
+    name: 'UsersPage',
+    components: {Users}
   }
 </script>
 
