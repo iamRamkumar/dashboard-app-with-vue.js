0206d6a98f697e39f160eff6d33f969735daf13e
Ramkumar <ramk2394@gmail.com>
2019-02-25 17:36:20 +0530

    Added router links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,9 +6,9 @@
       <b-navbar-brand href="#">Project Name</b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav class="ml-auto">
-          <b-nav-item active>Dashboard</b-nav-item>
-          <b-nav-item>Charts</b-nav-item>
-          <b-nav-item>Users</b-nav-item>
+          <b-nav-item :to="{ path: '/dashboard'}">Dashboard</b-nav-item>
+          <b-nav-item :to="{ path: '/charts'}">Charts</b-nav-item>
+          <b-nav-item :to="{ path: '/users'}">Users</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
