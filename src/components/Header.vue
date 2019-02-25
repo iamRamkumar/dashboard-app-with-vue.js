8eb03b80ed11dc4134d16366641574235cb4b886
Ramkumar <ramk2394@gmail.com>
2019-02-25 12:59:51 +0530

    Added Header and SideBar

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-toggle target="nav_collapse" />
+
+      <b-navbar-brand href="#">Project Name</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item active>Dashboard</b-nav-item>
+          <b-nav-item>Charts</b-nav-item>
+          <b-nav-item>Users</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ src/components/SideNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <!--<div>-->
+  <b-nav pills>
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>Comp 1</b-nav-item>
+    <b-nav-item>Comp 2</b-nav-item>
+    <b-nav-item>Comp 3</b-nav-item>
+  </b-nav>
+  <!--</div>-->
+</template>
+
+<script>
+  export default {
+    name: 'SideNav'
+  }
+</script>
+
+<style scoped>
+
+</style>
