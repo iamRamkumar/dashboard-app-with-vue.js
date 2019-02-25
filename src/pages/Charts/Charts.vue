07d2b3c3e6f2f3ba46b87874633a47f881e45c86
Ramkumar <ramk2394@gmail.com>
2019-02-25 13:59:52 +0530

    added Chart view page

---
+++ src/pages/Charts/Charts.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="charts">
+    <b-row>
+      <b-col>
+        <h1>Charts</h1>
+      </b-col>
+      <div class="w-100"></div>
+      <b-col>
+        <ColumnChart />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+  import ColumnChart from '@/components/ColumnChart'
+  export default {
+    name: 'Charts',
+    components: {ColumnChart}
+  }
+</script>
+
+<style scoped>
+
+</style>
