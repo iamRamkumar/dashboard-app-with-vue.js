68a869e53e9df37436cee34b0526c74d90ad4243
Ramkumar <ramk2394@gmail.com>
2019-02-26 15:53:26 +0530

    Added Icon to Navbar items

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,9 +6,9 @@
       <b-navbar-brand href="#">Project Name</b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav class="ml-auto">
-          <b-nav-item :to="{ path: '/dashboard'}">Dashboard</b-nav-item>
-          <b-nav-item :to="{ path: '/charts'}">Charts</b-nav-item>
-          <b-nav-item :to="{ path: '/users'}">Users</b-nav-item>
+          <b-nav-item :to="{ path: '/dashboard'}"><font-awesome-icon :icon="dashboardIcon"></font-awesome-icon> Dashboard</b-nav-item>
+          <b-nav-item :to="{ path: '/charts'}"><font-awesome-icon :icon="chartIcon"></font-awesome-icon> Charts</b-nav-item>
+          <b-nav-item :to="{ path: '/users'}"><font-awesome-icon :icon="usersIcon"></font-awesome-icon> Users</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -16,12 +16,23 @@
 </template>
 
 <script>
-  export default {
-    name: 'Header'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTachometerAlt, faChartLine, faUsers } from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  name: 'Header',
+  components: {FontAwesomeIcon},
+  data: function () {
+    return {
+      dashboardIcon: faTachometerAlt,
+      chartIcon: faChartLine,
+      usersIcon: faUsers
+    }
   }
+
+}
 </script>
 
 <style scoped>
 
 </style>
-
