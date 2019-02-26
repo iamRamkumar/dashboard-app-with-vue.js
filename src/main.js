9414499cab8c83f5a9a9bd6665acdd28c9ef97ee
Ramkumar <ramk2394@gmail.com>
2019-02-26 15:55:57 +0530

    Added Font-awesome Component to app

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import App from './App'
 import router from './router'
 
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVue)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
