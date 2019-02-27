86d80ef1bf5bab69be29dbd1f0dda34ac5d2d99b
Ramkumar <ramk2394@gmail.com>
2019-02-27 14:02:11 +0530

    prevented unauthorised access to the restricted routes

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,8 @@ import LoginForm from '@/pages/Login/LoginForm'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -45,6 +46,22 @@ export default new Router({
       path: '/login',
       name: 'LoginForm',
       component: LoginForm
-    }
+    },
+    { path: '*', redirect: '/' }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login']
+  const authRequired = !publicPages.includes(to.path)
+  const loggedIn = localStorage.getItem('user')
+
+  if (authRequired && !loggedIn) {
+    return next('/login')
+  }
+
+  next()
+})
+
+export default router
