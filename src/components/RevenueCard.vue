652f4da7077c060e09e8a34ecb4e5a98fc91646f
Ramkumar <ramk2394@gmail.com>
2019-02-27 10:49:40 +0530

    Splitted Styles of revenue cards

--- src/components/RevenueCard.vue
+++ src/components/RevenueCard.vue
@@ -39,5 +39,19 @@ export default {
 </script>
 
 <style scoped>
+.revenue-card-body{
+  /*background-image: linear-gradient(to bottom, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0.6) 100%), url("../assets/aeroplane.svg");*/
+  background: url("../assets/money-bag.svg");
+  background-repeat: no-repeat;
+  background-position: right;
+  background-blend-mode: hue;
+  background-size: 27%;
+  /*opacity:0.4;*/
+}
 
+.revenue-card {
+  background: #C02425;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #F0CB35, #C02425);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #F0CB35, #C02425); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
 </style>
--- src/components/SegmentCard.vue
+++ src/components/SegmentCard.vue
@@ -41,5 +41,19 @@ export default {
 </script>
 
 <style scoped>
+  .segment-card{
+    background: #5C258D;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to left, #4389A2, #5C258D);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to left, #4389A2, #5C258D); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
 
+  .segment-card-body{
+    /*background-image: linear-gradient(to bottom, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0.6) 100%), url("../assets/aeroplane.svg");*/
+    background: url("../assets/aeroplane.svg");
+    background-repeat: no-repeat;
+    background-position: right;
+    background-blend-mode: hue;
+    background-size: 27%;
+    /*opacity:0.4;*/
+  }
 </style>
