2f8a68c5a694763d6f24c0544eb2c0e56a350687
Ramkumar <ramk2394@gmail.com>
2019-02-28 10:24:53 +0530

    Changed contraint check to show navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="userName">
+  <div v-if="isLogin">
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-toggle target="nav_collapse" />
       <b-navbar-brand href="#">Project Name</b-navbar-brand>
@@ -23,7 +23,7 @@ import { faTachometerAlt, faChartLine, faTable, faUniversity, faUser } from '@fo
 export default {
   name: 'Header',
   components: {FontAwesomeIcon},
-  props: ['userName'],
+  props: ['userName', 'isLogin'],
   data: function () {
     return {
       dashboardIcon: faTachometerAlt,
