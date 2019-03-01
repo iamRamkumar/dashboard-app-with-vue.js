f064d80ee0a6ff28553d6311d6748e63e142af3d
Ramkumar <ramk2394@gmail.com>
2019-03-01 15:26:35 +0530

    Added Charts for Revenue and Client Graph

---
+++ src/components/ChartJSExample.vue
@@ -0,0 +1,25 @@
+<script>
+import {Bar} from 'vue-chartjs'
+
+export default {
+  name: 'ChartJSExample',
+  extends: Bar,
+  props: {
+    chartData: {
+      type: Object,
+      default: null
+    },
+    chartOptions: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.chartOptions)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MonthRevenue.vue
@@ -0,0 +1,24 @@
+<script>
+import {Line} from 'vue-chartjs'
+export default {
+  name: 'MonthRevenue',
+  extends: Line,
+  props: {
+    chartData: {
+      type: Object,
+      default: null
+    },
+    chartOptions: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.chartOptions)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
