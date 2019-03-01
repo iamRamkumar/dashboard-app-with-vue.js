7e9843237cf0fe707f364a45fbce9e00676823a8
Ramkumar <ramk2394@gmail.com>
2019-03-01 15:22:48 +0530

    Added Sample Data

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,3 +2,181 @@ export const USER = {
   username: 'Ramkumar',
   password: 'Ram123|!'
 }
+
+export const SAMPLEDATA = {
+  dashboarddata: {
+    totalsegments: 125475854,
+    totalbookings: 12547555,
+    totalrevenueamt: 8548555,
+    revenuePerDay: [
+      {
+        revenue: 1438.08,
+        date: '01-01-2019'
+      },
+      {
+        revenue: 1845.07,
+        date: '02-01-2019'
+      },
+      {
+        revenue: 3933.21,
+        date: '03-01-2019'
+      },
+      {
+        revenue: 2555.08,
+        date: '04-01-2019'
+      },
+      {
+        revenue: 2609.47,
+        date: '05-01-2019'
+      },
+      {
+        revenue: 1560.53,
+        date: '06-01-2019'
+      },
+      {
+        revenue: 1048.53,
+        date: '07-01-2019'
+      },
+      {
+        revenue: 2796.87,
+        date: '08-01-2019'
+      },
+      {
+        revenue: 3079.33,
+        date: '09-01-2019'
+      },
+      {
+        revenue: 2076.99,
+        date: '10-01-2019'
+      },
+      {
+        revenue: 2748.09,
+        date: '11-01-2019'
+      },
+      {
+        revenue: 1390.30,
+        date: '12-01-2019'
+      },
+      {
+        revenue: 2076.99,
+        date: '13-01-2019'
+      },
+      {
+        revenue: 2076.99,
+        date: '14-01-2019'
+      },
+      {
+        revenue: 2748.09,
+        date: '15-01-2019'
+      },
+      {
+        revenue: 2243.84,
+        date: '16-01-2019'
+      },
+      {
+        revenue: 1390.30,
+        date: '17-01-2019'
+      },
+      {
+        revenue: 3803.67,
+        date: '18-01-2019'
+      },
+      {
+        revenue: 1501.96,
+        date: '19-01-2019'
+      },
+      {
+        revenue: 3595.03,
+        date: '20-01-2019'
+      },
+      {
+        revenue: 1700.89,
+        date: '21-01-2019'
+      },
+      {
+        revenue: 1687.65,
+        date: '22-01-2019'
+      },
+      {
+        revenue: 2925.79,
+        date: '23-01-2019'
+      },
+      {
+        revenue: 1479.31,
+        date: '24-01-2019'
+      },
+      {
+        revenue: 1948.81,
+        date: '25-01-2019'
+      },
+      {
+        revenue: 3474.05,
+        date: '26-01-2019'
+      },
+      {
+        revenue: 2494.86,
+        date: '27-01-2019'
+      },
+      {
+        revenue: 2561.53,
+        date: '28-01-2019'
+      },
+      {
+        revenue: 1886.80,
+        date: '29-01-2019'
+      },
+      {
+        revenue: 3057.50,
+        date: '30-01-2019'
+      }
+    ],
+    currenycode: 'USD',
+    bookingsperday: [
+      {
+        client: 'malindo',
+        totalbookings: 154758,
+        totalsegments: 475848,
+        totalrevenueamt: 475848,
+        channels: [
+          {
+            channelcode: 'B2C',
+            todaybookings: 45,
+            yesterdaybookings: 45874
+          },
+          {
+            channelcode: 'B2B',
+            todaybookings: 874,
+            yesterdaybookings: 45871
+          }
+        ]
+      },
+      {
+        client: 'thailion',
+        totalbookings: 74854,
+        totalsegments: 45744,
+        totalrevenueamt: 78544,
+        channels: [
+          {
+            channelcode: 'B2C',
+            todaybookings: 45,
+            yesterdaybookings: 45874
+          },
+          {
+            channelcode: 'B2B',
+            todaybookings: 874,
+            yesterdaybookings: 45871
+          }
+        ]
+      }
+    ]
+  },
+  result: {
+    issuccess: true,
+    message: [
+      {
+        errorcode: 1,
+        errormsg: 'server error'
+      }
+    ]
+  }
+}
